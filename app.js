a1ccb6dabbeddc492f1c976a18a9d395c94449b2
Sajjad <65295591+Sajjad-Mi@users.noreply.github.com>
2023-02-12 22:12:40 +0330

    feat: add authorization for subscription routes

--- app.js
+++ app.js
@@ -3,7 +3,12 @@ const app = express();
 const connection = require('./db');
 
 const authRoutes = require('./Routes/Auth');
+const subscriptionRoutes = require('./Routes/Subscription')
 const userTable = require('./Models/UserSchema');
+const subsPlanTable = require('./Models/SubscriptionPlansSchema');
+const subsUserTable = require('./Models/UserSubscriptionSchema');
+const {checkAuthorization} = require('./middleware/Auth')
+
 const cookieParser = require('cookie-parser');
 
 require('dotenv').config();
@@ -14,10 +19,19 @@ app.use(express.json());
 const PORT = process.env.PORT || 8080;
 
 const main = async () =>{
-  const createUserTable = await connection.query(userTable)
-  app.listen(PORT, () => console.log(`server listening in port ${PORT}`))
+  try {
+    const createUserTable = await connection.query(userTable);
+    const createSubsTable = await connection.query(subsPlanTable);
+    const createUserSubsTable = await connection.query(subsUserTable);
+  
+    app.listen(PORT, () => console.log(`server listening in port ${PORT}`));
+  } catch (err) {
+    console.log(err.message)
+  }
+ 
 }
 
 main();
 
 app.use(authRoutes);
+app.use(checkAuthorization, subscriptionRoutes);
