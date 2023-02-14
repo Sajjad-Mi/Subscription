01434f3d7a01258c1c6c967b338dd437a9bca50f
Sajjad <65295591+Sajjad-Mi@users.noreply.github.com>
2023-02-14 11:51:37 +0330

    fix: change price and credit type to decimal

--- Models/SubscriptionPlansSchema.js
+++ Models/SubscriptionPlansSchema.js
@@ -3,7 +3,7 @@ const subsPlanTable = `
     CREATE TABLE if not exists subscription_plan (
         name VARCHAR(255) PRIMARY KEY ,
         duration INTEGER NOT NULL,
-        price INTEGER  NOT NULL
+        price DECIMAL(15,2) NOT NULL
     );
 `
 module.exports = subsPlanTable;
--- Models/UserSchema.js
+++ Models/UserSchema.js
@@ -3,7 +3,7 @@ const userTable = `
     CREATE TABLE if not exists users (
         id INTEGER PRIMARY KEY AUTO_INCREMENT,
         username VARCHAR(255) NOT NULL UNIQUE,
-        credit INTEGER  NOT NULL
+        credit DECIMAL(15,2) NOT NULL
     );
 `
 module.exports = userTable;
